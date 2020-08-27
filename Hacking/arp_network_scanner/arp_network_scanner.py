86c98f69ef46a8e9ae885a90ffd48337114c963d
RaffaDNDM <dinardodimaioraffaele@gmail.com>
2020-08-27 22:50:59 +0200

    Check of input network address.

--- Hacking/arp_network_scanner/arp_network_scanner.py
+++ Hacking/arp_network_scanner/arp_network_scanner.py
@@ -7,8 +7,60 @@ you need to import each module separately
 from scapy.layers.l2 import arping, ARP, Ether, srp
 from scapy.layers.inet import IP, UDP, TCP, ICMP
 from termcolor import cprint
+import argparse
+from sys import exit, getsizeof
+import struct
 
 BROADCAST_MAC = 'ff:ff:ff:ff:ff:ff'
+IP_MASK = 0x80000000
+
+class NoNetworkSpecified(Exception):
+    pass
+
+def check_format_IP(network):
+    #params[0]=IP 
+    #params[1]=number of bits of netmask
+    params = network.split('/')
+   
+    if not len(params)==2 or int(params[1])>32 or int(params[1])<=0:
+        raise NoNetworkSpecified
+
+    IP_numbers = params[0].split('.')
+    
+    if len(IP_numbers)!=4:
+        raise NoNetworkSpecified
+    else:
+        for num in IP_numbers:
+            if(int(num)>255 or int(num)<0):
+                raise NoNetworkSpecified
+
+    cprint('\n   Netmask:  ', 'blue', attrs=['bold',], end='')
+    print(socket.inet_ntoa(struct.pack(">I", (0xffffffff << (32 - int(params[1]))) & 0xffffffff)))
+    cprint('IP address:  ', 'yellow', attrs=['bold',], end='')
+    print(f'{params[0]}', end='\n\n')
+
+    return network
+        
+
+def args_parser():
+    #Parser of command line arguments
+    parser = argparse.ArgumentParser()
+    #Initialization of needed arguments
+    parser.add_argument("-ip", "-net", dest="net", help="IP address of the network in format: x.x.x.x/netmask")
+    #Parse command line arguments
+    args = parser.parse_args()
+    
+    #Check if the arguments have been specified on command line
+    try:
+        if not args.net:
+            raise NoNetworkSpecified
+        
+        network=check_format_IP(args.net)
+    except NoNetworkSpecified:
+        parser.print_help()
+        exit(0)
+
+    return network
 
 def network_scan(network):
     #Create ARP request
@@ -36,13 +88,16 @@ def network_scan(network):
         cprint(" {:^18} ".format(response[1].hwsrc), 'yellow', end='')
         cprint("|", 'red')
     
-    cprint(' ______________________________________', 'red', end='\n\n')
+    cprint('|_________________|____________________|', 'red', end='\n\n')
+
 
 def main():
+    network = args_parser()    
     #Simplest way to implement ARP network scanner 
     #arping("192.168.1.1/24")
     #Implementation of ARP network scanner from zero
-    network_scan("192.168.1.1/24")
+    network_scan(network)
+
 
 if __name__=="__main__":
     main()
