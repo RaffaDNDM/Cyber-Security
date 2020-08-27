a5a412dd579b8db218299068750d5697ac9eb97d
RaffaDNDM <dinardodimaioraffaele@gmail.com>
2020-08-27 19:54:56 +0200

    Implemented network scanner for evaluation of MAC addresses through ARP.

--- Hacking/arp_network_scanner/arp_network_scanner.py
+++ Hacking/arp_network_scanner/arp_network_scanner.py
@@ -4,10 +4,45 @@ Because of how pylint works, you cannot type directly
 import scapy.all as scapy
 you need to import each module separately
 '''
-from scapy.layers.l2 import arping
+from scapy.layers.l2 import arping, ARP, Ether, srp
 from scapy.layers.inet import IP, UDP, TCP, ICMP
+from termcolor import cprint
 
-#Simplest way to implement ARP network scanner 
-arping("192.168.1.1/24")
+BROADCAST_MAC = 'ff:ff:ff:ff:ff:ff'
 
-#Implementation of ARP network scanner from zero
+def network_scan(network):
+    #Create ARP request
+    arp_head = ARP(pdst=network)
+    ether_head = Ether(dst=BROADCAST_MAC)
+    request = ether_head/arp_head
+    
+    #Send request and wait for response
+    responses_list = srp(request, timeout=1)[0]
+
+    cprint(' ______________________________________', 'red')
+    cprint("|", 'red', end='')
+    cprint(" {:^15} ".format('IP address'), 'blue', 'on_green', end='')
+    cprint("|", 'red', end='')
+    cprint(" {:^18} ".format('MAC address'), 'blue', 'on_yellow', end='')
+    cprint("|", 'red')
+
+    for response in responses_list:
+        #response[0]= packet sent
+        #response[1]= response
+        #print(response.show()) to see all fields of the response packet
+        cprint("|", 'red', end='')
+        cprint(" {:^15} ".format(response[1].psrc), 'green', end='')
+        cprint("|", 'red',end='')
+        cprint(" {:^18} ".format(response[1].hwsrc), 'yellow', end='')
+        cprint("|", 'red')
+    
+    cprint(' ______________________________________', 'red', end='\n\n')
+
+def main():
+    #Simplest way to implement ARP network scanner 
+    #arping("192.168.1.1/24")
+    #Implementation of ARP network scanner from zero
+    network_scan("192.168.1.1/24")
+
+if __name__=="__main__":
+    main()
